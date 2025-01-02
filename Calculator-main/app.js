fb42ee271da50225ff6334e1c229d79c1e6c87ce
peterstphen1 <peterstphen6@gmail.com>
2025-01-02 12:39:17 +0100

    calculator project

---
+++ Calculator-main/app.js
@@ -0,0 +1,100 @@
+let newInput = document.querySelector(".newInput");
+let lastInput = document.querySelector(".lastInput");
+let buttons = document.querySelector(".buttons").children;
+const deleteLastDigit = document.querySelector(".del");
+const clrAll = document.querySelector(".clear");
+const result = document.querySelector(".equal");
+const squareDigit = document.querySelector(".square");
+const squareRootDigit = document.querySelector(".squareRoot");
+const percentage = document.querySelector(".percentage");
+
+
+
+for(let button of buttons){
+  button.addEventListener('click', () => {
+    newInput.textContent += button.value
+
+    
+  })
+}
+
+
+function del() {
+  let deleteDigit = newInput.textContent;
+  newInput.textContent = deleteDigit.slice(0, -1);
+}
+deleteLastDigit.addEventListener("click", del);
+
+
+function clear() {
+  newInput.textContent = "";
+  lastInput.textContent = "";
+  
+}
+clrAll.addEventListener("click", clear);
+
+
+function equality() {
+  switch (true) {
+    case newInput.textContent.includes("+"):
+      let plusOperation = newInput.textContent;
+      let plusOperation2 = plusOperation.split("+");
+      let plusResult = plusOperation2.reduce((accumulator, currentValue) => {
+        return Number(accumulator) + Number(currentValue);
+      });
+      lastInput.textContent = plusResult;
+      newInput.textContent = "";
+      break;
+
+    case newInput.textContent.includes("-"):
+      let minusOperation = newInput.textContent;
+      let minusOperation2 = minusOperation.split("-");
+      let minusResult = minusOperation2.reduce((accumulator, currentValue) => {
+        return Number(accumulator) - Number(currentValue);
+      });
+      lastInput.textContent = minusResult;
+      newInput.textContent = "";
+      break;
+
+    case newInput.textContent.includes("×"):
+      let multiplyOperation = newInput.textContent;
+      let multiplyOperation2 = multiplyOperation.split("×");
+      let multiplyResult = multiplyOperation2.reduce(
+        (accumulator, currentValue) => {
+          return Number(accumulator) * Number(currentValue);
+        }
+      );
+      lastInput.textContent = multiplyResult;
+      newInput.textContent = "";
+      break;
+
+    case newInput.textContent.includes("÷"):
+      let divideOperation = newInput.textContent;
+      let divideOperation2 = divideOperation.split("÷");
+      let divideResult = divideOperation2.reduce(
+        (accumulator, currentValue) => {
+          return Number(accumulator) / Number(currentValue);
+        }
+      );
+      lastInput.textContent = divideResult;
+      newInput.textContent = "";
+      break;
+  }
+}
+result.addEventListener("click", equality);
+
+
+function powerOfTwo() {
+  lastInput.textContent = newInput.textContent ** 2;
+  
+  newInput.textContent = "";
+}
+squareDigit.addEventListener("click", powerOfTwo);
+
+
+function squareRootOfDigit() {
+  lastInput.textContent = Math.sqrt(newInput.textContent).toFixed(4);
+  newInput.textContent = "";
+}
+
+squareRootDigit.addEventListener("click", squareRootOfDigit);
